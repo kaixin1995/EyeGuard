55d2cc9781fa1ba5f2b5d1bf7184ac718f92bd56
kaixin1995 <admin@haokaikai.cn>
2025-07-29 13:11:27 +0800

    版本号更新

--- EyeGuard/Dal.cs
+++ EyeGuard/Dal.cs
@@ -17,7 +17,7 @@ namespace EyeGuard
         /// <summary>
         /// 版本号
         /// </summary>
-        public static string Edition = "3.2.5";
+        public static string Edition = "3.2.6";
 
 
         /// <summary>
